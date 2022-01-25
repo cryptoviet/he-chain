54163aec6f335cf82e26c5aa2f6b60661381028e
mutobui <mutobui@gmail.com>
2022-01-25 22:18:25 +0700

    add modules

---
+++ pallets/pool/src/lib.rs
@@ -0,0 +1,47 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::{
+		dispatch::{DispatchResult, DispatchResultWithPostInfo},
+		pallet_prelude::*,
+		sp_runtime::traits::{Hash, Zero},
+		traits::{Currency, ExistenceRequirement, Randomness},
+	};
+	use frame_system::pallet_prelude::*;
+	use sp_io::hashing::blake2_128;
+
+	// Struct, Enum
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	/// Configure the pallet by specifying the parameters and types it depends on.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// Because this pallet emits events, it depends on the runtime's definition of an event.
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+
+		type Currency: Currency<Self::AccountId>;
+	}
+
+	// Errors.
+	#[pallet::error]
+	pub enum Error<T> {}
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {}
+
+	// Storage
+	// #[pallet::storage]
+	// #[pallet::getter(fn )]
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {}
+
+	impl<T: Config> Pallet<T> {}
+}
